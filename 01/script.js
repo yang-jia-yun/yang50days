eae40fb57289ff69088ef4d99175f9f50af05452
yang <2296679531@qq.com>
2022-06-08 22:20:52 +0800

    update:update:1. 手风琴 2. 步进条

---
+++ 01/script.js
@@ -0,0 +1,16 @@
+// console.log(document.getElementsByClassName('panel'));
+
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) =>
+  panel.addEventListener("click", function () {
+    // document.querySelector('.panel.active').classList.remove('active')
+    clearActiveClasses();
+
+    panel.classList.add("active");
+  })
+);
+
+function clearActiveClasses() {
+  panels.forEach((panel) => panel.classList.remove("active"));
+}
---
+++ 02/script.js
@@ -0,0 +1,39 @@
+const circular = document.getElementsByClassName(".circular");
+let currentId = 1;
+const add = () => {
+  document.getElementById(`circular-${currentId}`).classList.add("active");
+
+  document.getElementsByClassName("line-active")[0].style.width =
+    (100 / 3) * (currentId - 1) + "%";
+};
+const remove = () => {
+  document.getElementById(`circular-${currentId+1}`).classList.remove("active");
+  document.getElementsByClassName("line-active")[0].style.width =
+    (100 / 3) * (currentId - 1) + "%";
+};
+
+document.getElementById("next").addEventListener("click", () => {
+    currentId++;
+    add();
+  if (currentId < 4) {
+      //
+    console.log(currentId);
+    document.querySelector("#prev").removeAttribute('disabled')
+
+  } else {
+    // document.querySelector("#next").removeAttribute('disabled')
+console.log('aaaaa');
+    document.querySelector("#next").setAttribute("disabled", true);
+  }
+});
+
+document.getElementById("prev").addEventListener("click", () => {
+    currentId--;
+    remove();
+  if (currentId < 5 && currentId > 1) {
+    document.querySelector("#next").removeAttribute('disabled')
+  }else{
+    document.querySelector("#prev").setAttribute("disabled", true);
+
+  }
+});
