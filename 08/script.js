a7729498a963ad79f36c801681ad8c07514c38e8
yang <yjy@drcloud.me>
2023-02-28 12:00:55 +0800

    添加登录页动画，判断必填未完成

--- 08/script.js
+++ 08/script.js
@@ -1,7 +1,41 @@
+const email = document.querySelector('.email')
 const input = document.querySelectorAll('input')
+const button = document.querySelector('.button')
+const text = document.querySelector('.text')
 input.forEach((res) => {
+	let item = res
 	res.addEventListener('focus', () => {
-		email.classList.add('ani')
+		item.classList.add('active')
+		item.previousSibling.childNodes.forEach((val) => {
+			val.classList.remove('blurAni')
+			val.classList.add('ani')
+		})
+	})
+	res.addEventListener('blur', () => {
+		item.classList.remove('active')
+		item.previousSibling.childNodes.forEach((val) => {
+			val.classList.remove('ani')
+			val.classList.add('blurAni')
+		})
 	})
 })
-const email = document.querySelector('.email')
+button.addEventListener('mousedown', () => {
+	button.classList.add('buttonActive')
+})
+button.addEventListener('mouseup', () => {
+	button.classList.remove('buttonActive')
+})
+button.addEventListener('mouseenter', () => {
+	button.classList.add('activeText')
+})
+//点击login
+button.addEventListener('click', () => {
+	input.forEach((item) => {
+		if (item.required && !item.innerHTML) {
+			
+		}
+	})
+})
+text.addEventListener('mouseenter', () => {
+	text.classList.add('activeText')
+})
