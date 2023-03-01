cc39bd44f9f021a4e3a12f1de39383ba189e8d22
yang <yjy@drcloud.me>
2023-03-01 10:40:21 +0800

    完善登录页交互动画

--- 08/README.md
+++ 08/README.md
@@ -1,1 +1,41 @@
+// JS获取当前元素的上/下一个兄弟级元素或其他元素
 
+* 得到s的全部子节点
+var chils = s.childNodes;
+
+* 得到s的父节点
+var par = s.parentNode;
+
+* 获得s的下一个兄弟节点
+var ns = s.nextSibling;
+
+* 得到s的上一个兄弟节点
+var ps = s.previousSibling;
+
+* 获得s的第一个子节点
+var fc = s.firstChild;
+
+* 获得s的最后一个子节点
+var lc = s.lastChild;
+
+一、鼠标事件：
+
+click：单击
+
+dblclick：双击
+
+mousedown：鼠标按下
+
+mouseup：鼠标抬起
+
+mouseover：鼠标悬浮
+
+mouseout：鼠标离开
+
+mousemove：鼠标移动
+
+mouseenter：鼠标进入
+
+mouseleave：鼠标离开
+
+三、transform: scale(0.9, 0.9); 缩放
--- 08/script.js
+++ 08/script.js
@@ -1,7 +1,57 @@
+const email = document.querySelector('.email')
 const input = document.querySelectorAll('input')
+const button = document.querySelector('.button')
+const text = document.querySelector('.text')
 input.forEach((res) => {
+	let item = res
 	res.addEventListener('focus', () => {
-		email.classList.add('ani')
+		item.classList.add('active')
+		item.previousSibling.childNodes.forEach((val) => {
+			val.classList.remove('blurAni')
+			val.classList.add('ani')
+		})
+	})
+	res.addEventListener('blur', () => {
+		if (!res.value) {
+			item.classList.remove('active')
+			item.previousSibling.childNodes.forEach((val) => {
+				val.classList.remove('ani')
+				val.classList.add('blurAni')
+			})
+		}
+		let req = document.getElementById('requiredTips')
+		if (req) req.parentNode.removeChild(req)
 	})
 })
-const email = document.querySelector('.email')
+button.addEventListener('mousedown', () => {
+	button.classList.add('buttonActive')
+})
+button.addEventListener('mouseup', () => {
+	button.classList.remove('buttonActive')
+})
+button.addEventListener('mouseenter', () => {
+	button.classList.add('activeText')
+})
+//点击login
+button.addEventListener('click', () => {
+	let item = document.createElement('div')
+	item.innerText = '请填写此项'
+	item.classList.add('requiredTips')
+	item.id = 'requiredTips'
+	// input.forEach((val) => {
+	// 	if (val.required && !val.innerHTML) {
+	// 		val.parentNode.appendChild(item)
+	// 		return val
+	// 	}
+	// })
+	for (let val = 0; val < input.length; val++) {
+		if (input[val].required && !input[val].value) {
+			input[val].parentNode.appendChild(item)
+			input[val].focus()
+			return
+		}
+	}
+})
+text.addEventListener('mouseenter', () => {
+	text.classList.add('activeText')
+})
